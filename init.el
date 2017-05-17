edd82277a884e13ea550d247154af7198a43acd1
Sishaar Rao <sishaar@gmail.com>
2017-05-17 18:02:57 -0400

    init file

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 ;; Using C-l to clear the buffer in Eshell
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun eshell-clear-buffer ()
   "Clear terminal"
   (interactive)
@@ -37,11 +44,16 @@
 (setq wg-prefix-key (kbd "C-c w"))
 (workgroups-mode 1)
 
+;;Trying to get TeX to work
+(setenv "PATH" (concat (getenv "PATH") ":/Library/TeX/texbin/"))  
+(setq exec-path (append exec-path '("/Library/TeX/texbin/")))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (auctex)))
  '(send-mail-function (quote smtpmail-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
